50033137bec232c6658d6746071ade1f365f3058
Shripad SHIRSAT <Shripad.SHIRSAT@cotecna.co.in>
2022-07-07 13:10:46 +0530

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "test";
+            char[] arr = a.ToCharArray();
+            char[] resarr = new char[arr.Length];
+
+            for(int i=0, j = a.Length-1; i<a.Length -1; i++,j--)
+            {
+                resarr[i] = arr[j];
+            }
+
+        }
+    }
+}
